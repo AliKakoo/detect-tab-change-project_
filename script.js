224a1f5ef15b8fc3488b832f99333c6c7db4eea0
AliKakoo <alikakuo@yahoo.com>
2025-02-16 23:03:10 +0330

    add latest codes

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const $ = document
+const $ = document;
+
+const projectState = $.querySelector('span')
+
+$.addEventListener("visibilitychange", () => {
+  if ($.visibilityState === 'visible') {
+    $.title ="Active Tab "
+    
+} else {
+    
+    $.title ="InActive Tab "
+
+
+  }
+
+  projectState.innerHTML = '(Faild)'
+  projectState.classList.add('faild')
+
+});
